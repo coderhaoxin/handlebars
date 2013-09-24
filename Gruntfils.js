8d5c2f86e84af6dcfd4b53748f882d7a9b72e32c
haoxin <coderhaoxin@outlook.com>
2013-09-24 09:07:57 +0800

    add

--- Gruntfils.js
+++ Gruntfils.js
@@ -2,6 +2,28 @@ module.exports = function (grunt) {
   'use strict';
   grunt.initConfig({
     meta: {},
+    nodemon: {
+      prod: {
+        options: {
+          file: 'app.js',
+          args: ['develop'],
+          nodeArgs: [],
+          ignoredFiles: ['README.md', 'node_modules/**'],
+          watchedExtensions: ['js'],
+          watchedFolders: ['service', 'util'],
+          delayTime: 1,
+          env: {
+            PORT: '3000'
+          },
+          cwd: __dirname
+        }
+      },
+      exec: {
+        options: {
+          exec: 'less'
+        }
+      }
+    },
     concat: {
       js: {
         src: 'static-dev/js/*.js',
@@ -56,14 +78,24 @@ module.exports = function (grunt) {
       }
     }
   });
+
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   grunt.registerTask('develop', ['concat', 'uglify', 'jshint']);
 }
