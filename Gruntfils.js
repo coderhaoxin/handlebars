c1026117bd6e5f82c58a3016109328d00e5ec711
haoxin <coderhaoxin@outlook.com>
2013-09-25 18:37:38 +0800

    add

--- Gruntfils.js
+++ Gruntfils.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
   'use strict';
   grunt.initConfig({
     meta: {},
+    //
     nodemon: {
       prod: {
         options: {
@@ -24,6 +25,7 @@ module.exports = function (grunt) {
         }
       }
     },
+    //concat js,css files
     concat: {
       js: {
         src: 'static-dev/js/*.js',
@@ -34,6 +36,7 @@ module.exports = function (grunt) {
         dest: 'static/css/app.css'
       }
     },
+    //min js files
     uglify: {
       options: {
         mangle: false
@@ -44,37 +47,44 @@ module.exports = function (grunt) {
         }
       }
     },
+    //
     jshint: {
       beforeconcat: ['static-dev/js/*.js'],
       afterconcat: ['static/js/app.js']
     },
+    //
     jasmine: {
       src: 'static-dev/js/*.js',
       options: {
         specs: 'test/static/*.js',
       }
     },
+    //
     csslint: {
       src: 'static/css/app.css'
     },
+    //
     cssmin: {
       css: {
         src: 'static/css/app.css',
         dest: 'static/css/app.min.css'
       }
     },
+    //
     watch: {
-      concat_js_css: {
-        files: ['public/app/*/*.js', 'public/app/*/*.css'],
-        tasks: ['concat']
-      },
-      jshint_jsmin: {
+      jshint: {
         files: ['static/js/app.js'],
-        tasks: ['jshint', 'uglify']
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
       },
-      csslint_cssmin: {
+      csslint: {
         files: ['static/css/app.css'],
-        tasks: ['csslint', 'cssmin']
+        tasks: ['csslint'],
+        options: {
+          livereload: true
+        }
       }
     }
   });
@@ -98,4 +108,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('develop', ['concat', 'uglify', 'jshint']);
+  grunt.registerTask('release', ['']);
 }
