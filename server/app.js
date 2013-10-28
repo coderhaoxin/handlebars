0afc95bb6a47e6b05f2ff78c7f7807bdb728cef7
haoxin <coderhaoxin@outlook.com>
2013-10-28 09:09:31 +0800

    new branch init

--- server/app.js
+++ server/app.js
@@ -9,13 +9,6 @@ if (!fs.existsSync(__dirname + '/upload')) {
   fs.mkdirSync('upload')
 }
 
-//mongodb(mongoose)
-var mongoose = require('mongoose')
-mongoose.connect(config.mongo.connectUrl)
-var mongoConnection = mongoose.connection
-mongoConnection.on('error', console.error.bind(console, 'mongodb connection error:'))
-mongoConnection.once('open', function () { console.log('mongodb connection open') })
-
 //express
 var express = require('express')
 var app     = express()
