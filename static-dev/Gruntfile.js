3719ddcdbdf0c2582107271683de4e6646f78a8e
haoxin <coderhaoxin@outlook.com>
2013-10-29 17:55:08 +0800

    add something

--- static-dev/Gruntfile.js
+++ static-dev/Gruntfile.js
@@ -26,39 +26,43 @@ module.exports = function (grunt) {
     },
     concat: {
       js: {
-        src: 'static-dev/js/*.js',
-        dest: 'static/js/app.js'
+        src: 'js/*.js',
+        dest: '../static/js/app.js'
       },
       css: {
-        src: 'static-dev/css/*.css',
-        dest: 'static/css/app.css'
+        src: 'css/*.css',
+        dest: '../static/css/app.css'
       }
     },
     jasmine: { //测试应该测试release后的，而非dev的
-      src: 'static/js/app.js',
+      src: '../static/js/app.js',
       options: {
-        specs: 'static-dev/test/*.js',
+        specs: 'test/*.js',
       }
     },
     copy: {
       lib: {
         files: [
-          { src: 'static-dev/lib/angular/angular.js',     dest: 'static-dev/lib/angular.js' },
-          { src: 'static-dev/lib/angular/angular.min.js', dest: 'static/lib/angular.min.js' },
-          { src: 'static-dev/lib/bootstrap/dist/css/bootstrap.css',     dest: 'static-dev/lib/bootstrap.css' },
-          { src: 'static-dev/lib/bootstrap/dist/css/bootstrap.min.css', dest: 'static/lib/bootstrap.min.css' },
-          { src: 'static-dev/lib/bootstrap/dist/js/bootstrap.js',       dest: 'static-dev/lib/bootstrap.js' },
-          { src: 'static-dev/lib/bootstrap/dist/js/bootstrap.min.js',   dest: 'static/lib/bootstrap.min.js' },
-          { src: 'static-dev/lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: 'static-dev/lib/jquery.fileupload-angular.js' },
-          { src: 'static-dev/lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: 'static/lib/jquery.fileupload-angular.js' },
-          { src: 'static-dev/lib/jquery/jquery.js',     dest: 'static-dev/lib/jquery.js' },
-          { src: 'static-dev/lib/jquery/jquery.min.js', dest: 'static/lib/jquery.min.js' }
+          { src: 'lib/angular/angular.js',     dest: 'lib/angular.js' },
+          { src: 'lib/angular/angular.min.js', dest: '../static/lib/angular.min.js' },
+          { src: 'lib/bootstrap/dist/css/bootstrap.css',     dest: 'lib/bootstrap.css' },
+          { src: 'lib/bootstrap/dist/css/bootstrap.min.css', dest: '../static/lib/bootstrap.min.css' },
+          { src: 'lib/bootstrap/dist/js/bootstrap.js',       dest: 'lib/bootstrap.js' },
+          { src: 'lib/bootstrap/dist/js/bootstrap.min.js',   dest: '../static/lib/bootstrap.min.js' },
+          { src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: 'lib/jquery.fileupload-angular.js' },
+          { src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: '../static/lib/jquery.fileupload-angular.js' },
+          { src: 'lib/jquery/jquery.js',     dest: 'lib/jquery.js' },
+          { src: 'lib/jquery/jquery.min.js', dest: '../static/lib/jquery.min.js' }
         ]
       }
     },
     watch: {
+      concat_js_css: {
+        files: ['js/*.js', 'css/*.css'],
+        tasks: ['concat']
+      },
       livereload: {
-        files: ['static/js/app.js', 'static/css/app.css'],
+        files: ['../static/js/app.js', '../static/css/app.css'],
         options: {
           livereload: true
         }
@@ -67,5 +71,4 @@ module.exports = function (grunt) {
   })
 
   grunt.registerTask('default', ['concurrent:watch'])
-  grunt.registerTask('release', ['concurrent:copy', 'concurrent:concat', 'concurrent:jasmine'])
 }
