7fd99a44729389821afbca5fb41c5e3367b7fb3a
haoxin <coderhaoxin@outlook.com>
2013-11-05 14:56:31 +0800

    add

--- static-dev/Gruntfile.js
+++ static-dev/Gruntfile.js
@@ -11,12 +11,6 @@ module.exports = function (grunt) {
           logConcurrentOutput: true
         }
       },
-      concat: {
-        tasks: ['concat'],
-        options: {
-          logConcurrentOutput: true
-        }
-      },
       jasmine: {
         tasks: ['jasmine'],
         options: {
@@ -25,15 +19,6 @@ module.exports = function (grunt) {
       }
     },
     concat: {
-      js: {
-        src: 'js/*.js',
-        dest: '../static/js/app.js'
-      },
-      css: {
-        src: 'css/*.css',
-        dest: '../static/css/app.css'
-      }
-    },
     jasmine: { //测试应该测试release后的，而非dev的
       src: '../static/js/app.js',
       options: {
@@ -42,25 +27,49 @@ module.exports = function (grunt) {
     },
     copy: {
       lib: {
-        files: [
-          { src: 'lib/angular/angular.js',     dest: 'lib/angular.js' },
-          { src: 'lib/angular/angular.min.js', dest: '../static/lib/angular.min.js' },
-          { src: 'lib/bootstrap/dist/css/bootstrap.css',     dest: 'lib/bootstrap.css' },
-          { src: 'lib/bootstrap/dist/css/bootstrap.min.css', dest: '../static/lib/bootstrap.min.css' },
-          { src: 'lib/bootstrap/dist/js/bootstrap.js',       dest: 'lib/bootstrap.js' },
-          { src: 'lib/bootstrap/dist/js/bootstrap.min.js',   dest: '../static/lib/bootstrap.min.js' },
-          { src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: 'lib/jquery.fileupload-angular.js' },
-          { src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js', dest: '../static/lib/jquery.fileupload-angular.js' },
-          { src: 'lib/jquery/jquery.js',     dest: 'lib/jquery.js' },
-          { src: 'lib/jquery/jquery.min.js', dest: '../static/lib/jquery.min.js' }
-        ]
+        files: [{
+          src: 'lib/angular/angular.js',
+          dest: 'lib/angular.js'
+        },{
+          src: 'lib/angular/angular.min.js',
+          dest: '../static/lib/angular.min.js'
+        },{
+          src: 'lib/bootstrap/dist/css/bootstrap.css',
+          dest: 'lib/bootstrap.css'
+        },{
+          src: 'lib/bootstrap/dist/css/bootstrap.min.css',
+          dest: '../static/lib/bootstrap.min.css'
+        },{
+          src: 'lib/bootstrap/dist/js/bootstrap.js',
+          dest: 'lib/bootstrap.js'
+        },{
+          src: 'lib/bootstrap/dist/js/bootstrap.min.js',
+          dest: '../static/lib/bootstrap.min.js'
+        },{
+          src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js',
+          dest: 'lib/jquery.fileupload-angular.js'
+        },{
+          src: 'lib/blueimp-file-upload/js/jquery.fileupload-angular.js',
+          dest: '../static/lib/jquery.fileupload-angular.js'
+        },{
+          src: 'lib/jquery/jquery.js',
+          dest: 'lib/jquery.js'
+        },{
+          src: 'lib/jquery/jquery.min.js',
+          dest: '../static/lib/jquery.min.js'
+        }]
       }
     },
+    clean: {
+      lib: ['lib/angular',
+            'lib/blueimp-canvas-to-blob',
+            'lib/blueimp-file-upload',
+            'lib/blueimp-load-image',
+            'lib/blueimp-tmpl',
+            'lib/bootstrap',
+            'lib/jquery']
+    },
     watch: {
-      concat_js_css: {
-        files: ['js/*.js', 'css/*.css'],
-        tasks: ['concat']
-      },
       livereload: {
         files: ['../static/js/app.js', '../static/css/app.css'],
         options: {
---
+++ static-dev/README.md
@@ -0,0 +1,13 @@
+### install
+
+```bash
+cd static-dev
+
+# install npm package
+npm install
+
+# install lib
+bower install
+grunt copy
+grunt clean
+```
