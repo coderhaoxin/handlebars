b8f3f1028d6aab251faa9aba78e8f9961930675f
haoxin <coderhaoxin@outlook.com>
2013-11-25 12:42:01 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var config  = require('./config')
 var express = require('express')
 var app     = express()
@@ -16,10 +17,13 @@ if (!fs.existsSync('./upload')) {
 * express
 */
 app.use(express.cookieParser())
+// app.use(express.cookieSession({ secret: 'node-app-seed', cookie: { maxAge: 15*24*60*60*1000 } }));
 app.use(express.bodyParser({ uploadDir: './upload' }))
+
 // redis session
 var RedisStore = require('connect-redis')(express)
 app.use(express.session({ secret: 'node-app-seed', store: new RedisStore(config.redis) }))
+
 // route
 route(app)
 
