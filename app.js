112b32cf5ac1af816730a0087ff1f1bdff15a53a
coderhaoxin <coderhaoxin@outlook.com>
2013-12-20 11:01:46 +0800

    fix: use session

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ if (!fs.existsSync('./upload')) {
 */
 app.use(etag())
 app.use(staticCache('./static', { maxAge: 1000 }))
-// app.use(session)
+app.keys = ['koa-cookie-key']
+app.use(session())
 
 /*
 * route
